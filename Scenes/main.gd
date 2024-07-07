868d82a9d8f0209313584d1c663649a135110f31
sosolikova <veronika.sosolikova@gmail.com>
2024-07-07 12:01:57 +0200

    Create movement variables

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -15,6 +15,16 @@ var old_data : Array
 var snake_data : Array
 var snake : Array
 
+# movement variables
+var start_pos = Vector2(9,9)
+var up = Vector2(0,-1)
+var down = Vector2(0,1)
+var left = Vector2(-1,0)
+var right = Vector2(1,0)
+var move_direction : Vector2
+var can_move: bool
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
